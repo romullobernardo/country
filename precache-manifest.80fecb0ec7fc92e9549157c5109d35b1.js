60cd4b505f4edcd37989d4fc94f9866f26d46586
Romullo Bernardo <romullobernardo@XRT25-22B.local>
2019-12-15 17:34:13 -0500

    Updates

---
+++ precache-manifest.80fecb0ec7fc92e9549157c5109d35b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10e70bdeee46086397429eb3ca0c843a",
+    "url": "/country/index.html"
+  },
+  {
+    "revision": "ff00a7448d6cda9c63ed",
+    "url": "/country/static/js/2.0648f4e1.chunk.js"
+  },
+  {
+    "revision": "6137c0223a1d48fe5f45",
+    "url": "/country/static/js/main.e01e22ee.chunk.js"
+  },
+  {
+    "revision": "750ae4c9767581bdcbda",
+    "url": "/country/static/js/runtime~main.0ae1b7f8.js"
+  },
+  {
+    "revision": "f5f3aebb4c9305662512960ce0307e35",
+    "url": "/country/static/media/background.f5f3aebb.svg"
+  }
+]);
